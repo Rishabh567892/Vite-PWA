c4bbd8256bc89a27c1ff95455472feb89ac47b56
Rishabh567892 <146851398+Rishabh567892@users.noreply.github.com>
2025-12-04 06:22:00 +0000

    new file:   .gitignore 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.svg 	new file:   pwa-assets.config.js 	new file:   src/counter.js 	new file:   src/javascript.svg 	new file:   src/main.js 	new file:   src/pwa.js 	new file:   src/style.css 	new file:   vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Vite-PWA',
+      short_name: 'Vite-PWA',
+      description: 'a very basic pwa application.',
+      theme_color: '#111111',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
